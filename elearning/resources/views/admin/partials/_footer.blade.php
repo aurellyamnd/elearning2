f30fe086fd7e31ddc5398a4a4ad2e08389856a3a
aurellyamnd <tasyaaurellya6@gmail.com>
2024-05-30 11:16:00 +0700

    tugas praktikum

---
+++ elearning/resources/views/admin/partials/_footer.blade.php
@@ -0,0 +1,5 @@
+<footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Tasya Aurellya</span></strong>. All Rights Reserved
+    </div>
+</footer><!-- End Footer -->
